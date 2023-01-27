7b6f1ad419c99faee409b1acce7732d16eb80bb5
Shadow-Zin <121673764+Shadow-Zin@users.noreply.github.com>
2023-01-27 15:09:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,23 +14,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['94764379595'] //ur owner number
+global.ownername = "No Name" //ur owner name
+global.ytname = "YT: Shadow del Zap" //ur yt chanel name
+global.socialm = "Instagram: devsshadow" //ur github or insta name
+global.location = "Sri Lanka" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
+global.ownernomer = "94764379595" //ur number
+global.premium = ['94764379595'] //ur premium number
 global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://www.helacloud.ga" //ur website to be displayed
+global.linkz = "https://instagram.com/devs_shadow" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/shadowdelzap" //ur website to be displayed
 global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
-global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.themeemoji = "✨" //ur theme emoji
+global.packname = "Sticker By Shadow Bot" //ur sticker watermark packname
+global.author = "Shadow Bot" //ur sticker watermark author
+global.wm = "Shadow Bot INC" //ur watermark
 
 // Other
 global.sessionName = 'session'
